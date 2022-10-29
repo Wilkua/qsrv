7c90db988e3fc591c65a5475edccc08aa32f6eb1
William Drescher <wilkua2@gmail.com>
2022-10-28 20:31:51 -0400

    Build more robust file server module

--- src/file_server.rs
+++ src/file_server.rs
@@ -1,5 +1,34 @@
+use crate::{HttpResponse, HttpRequest, Responder};
 use eyre::Result;
+use std::fs;
 use std::path::{Path, PathBuf};
+use tracing::{error, trace};
+
+fn mime_for_file_ext(path: &Path) -> String {
+    let mime = match path.extension() {
+        Some(e) => match e.to_str() {
+            Some(s) => match s {
+                "html" => "text/html",
+                "css" => "text/css",
+                "js" => "text/javascript",
+                "svg" => "image/svg+xml",
+                "jpg" => "image/jpeg",
+                "jpeg" => "image/jpeg",
+                "png" => "image/png",
+                _ => "application/octet-stream",
+            },
+            None => "application/octet-stream",
+        },
+        None => "application/octet-stream",
+    };
+    let mime = if mime.starts_with("text/") {
+        format!("{}; charset=utf-8", mime)
+    } else {
+        String::from(mime)
+    };
+
+    mime
+}
 
 pub struct FileServer {
     root_path: PathBuf,
@@ -8,6 +37,7 @@ pub struct FileServer {
 impl FileServer {
     pub fn new(root: &str) -> Result<Self> {
         let can_path = std::fs::canonicalize(Path::new(root))?;
+        trace!("root canonical path: {:?}", can_path.as_os_str());
 
         Ok(FileServer {
             root_path: can_path,
@@ -15,8 +45,41 @@ impl FileServer {
     }
 }
 
-impl super::Responder for FileServer {
-    fn handle_request(&self, _request: &super::HttpRequest) -> String {
-        String::from("FileServer works!")
+impl Responder for FileServer {
+    fn handle_request(&self, req: &HttpRequest) -> Result<HttpResponse> {
+        let mut working_path = PathBuf::from(&self.root_path);
+        working_path.push(&req.path[1..]);
+        if working_path.is_dir() {
+            trace!("requested directory - serving index");
+            working_path.push("index.html");
+        }
+        trace!("working request path: {:?}", working_path.as_os_str());
+        working_path = match fs::canonicalize(working_path) {
+            Ok(p) => p,
+            Err(e) => {
+                error!("Failed to canonicalize path: {}", e);
+                return Ok(HttpResponse::not_found(&req.http_version, &req.method));
+            },
+        };
+
+        if !working_path.starts_with(&self.root_path) {
+            return Ok(HttpResponse::forbidden(&req.http_version, &req.method));
+        }
+
+        if let Ok(buf) = fs::read(&working_path) {
+            let mime = mime_for_file_ext(&working_path);
+
+            let mut res = HttpResponse::new(&req.http_version, &req.method);
+
+            res.status = 200u16;
+            res.status_text = String::from("OK");
+            res.headers.insert(String::from("content-type"), mime);
+            res.headers.insert(String::from("content-length"), buf.len().to_string());
+            res.body = Some(buf);
+
+            return Ok(res);
+        }
+
+        Ok(HttpResponse::not_found(&req.http_version, &req.method))
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,142 +1,58 @@
 use eyre::Result;
-use qsrv::{HttpRequest, Responder};
-use qsrv::util::{
-    parse_headers,
-    parse_locator,
-    parse_method,
-    parse_path_components,
-    parse_version,
-};
-use std::collections::HashMap;
-use std::fs;
-use std::io::{Read, Write};
+use qsrv::{file_server::FileServer, HttpRequest, Responder};
+use std::io::Write;
 use std::net::{SocketAddr, TcpListener, TcpStream};
-use std::path::{Path, PathBuf};
 use std::thread;
+use tracing::{error, info, Level};
+use tracing_subscriber::fmt::Subscriber;
 
-pub fn respond_with_file(mut req: HttpRequest, path: &PathBuf) {
-    if let Ok(buf) = fs::read(path) {
-        let mime = match path.extension() {
-            Some(e) => match e.to_str() {
-                Some(s) => match s {
-                    "html" => "text/html",
-                    "css" => "text/css",
-                    "js" => "text/javascript",
-                    "svg" => "image/svg+xml",
-                    "jpg" => "image/jpeg",
-                    "jpeg" => "image/jpeg",
-                    "png" => "image/png",
-                    _ => "application/octet-stream",
-                },
-                None => "application/octet-stream",
-            },
-            None => "application/octet-stream",
-        };
-        let mime = if mime.starts_with("text/") {
-            format!("{}; charset=utf-8", mime)
-        } else {
-            String::from(mime)
-        };
-
-        let mut headers = HashMap::new();
-        headers.insert("content-type", mime);
-        headers.insert("content-length", buf.len().to_string());
-
-        let mut stream = &req.stream;
-
-        let _ = stream.write("HTTP/1.1 200 OK\r\n".as_bytes());
-        for (key, val) in headers {
-            let _ = stream.write(format!("{key}: {val}\r\n").as_bytes());
-        }
-        let _ = stream.write("\r\n".as_bytes());
-        let _ = stream.write(&buf);
-
-        println!("200 {} \"{}\" {} bytes", req.method, req.locator, buf.len());
-
-        return;
-    }
-
-    let _ = req.stream.write("HTTP/1.1 200 OK\r\nContent-Length: 0\r\n\r\n".as_bytes());
-
-    println!("404 {} \"{}\"", req.method, req.locator);
-}
-
-fn handle_request(mut stream: TcpStream) -> Result<()> {
-    let mut buf = [0u8; 2000];
-    let _ = stream.read(&mut buf);
-
+fn handle_request(stream: TcpStream) -> Result<()> {
     let mut req = HttpRequest::new(stream);
 
-    req.method = parse_method(&buf);
-    let (loc, next) = parse_locator(&buf, req.method.len() + 1);
-    req.locator = loc;
-    let (version, next) = parse_version(&buf, next + 1);
-    req.http_version = version;
-    req.headers = parse_headers(&buf, next + 1);
+    let file_server = FileServer::new(".")?;
+    let response = file_server.handle_request(&req)?;
 
-    let (path, _params) = parse_path_components(&req.locator.as_bytes());
-    req.path = path;
+    req.stream.write(&response.as_bytes())?;
 
-    let path_str = String::from(&req.path);
-    let mut path = PathBuf::new(); //Path::new(&path[1..]);
-    path.push(".");
-    path.push(Path::new(&path_str[1..]));
-
-    if path.exists() && path.is_dir() {
-        path.push("index.html");
-    }
-
-    if path.exists() && path.is_file() {
-        // Intended usage:
-        // let responder: FileServer = FileServer::new(".")?;
-        // if let Ok(()) = responder.handle_request(&req) {
-        //     return Ok(())
-        // }
-        // Here we search for a middleware handler
-        // if there is no middleware handler:
-        //     return 404
-
-        respond_with_file(req, &path);
-
-        return Ok(());
-    }
-
-    let _ = req.stream.write(
-        format!("{} 404 Not Found\r\nContent-Length: 9\r\nContent-Type: text/plain\r\n\r\nNot Found", req.http_version).as_bytes()
-    );
-
-    println!("404 {} \"{}\"", req.method, req.path);
+    info!("{} {} \"{}\" {} bytes",
+        &response.status,
+        &response.method,
+        &req.path,
+        match &response.body {
+            Some(b) => b.len(),
+            None => 0,
+        });
 
     Ok(())
 }
 
-/* enum Work { */
-/*     Quit, */
-/*     Request(HttpRequest), */
-/* } */
+fn main() -> Result<()> {
+    let subscriber = Subscriber::builder()
+        .with_max_level(Level::TRACE)
+        .finish();
 
-fn main() {
-    /* let mut queue: WorkQueue<Work> = WorkQueue::new(4, || { */
-    /* }); */
+    tracing::subscriber::set_global_default(subscriber)?;
 
     let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
-    let listener = match TcpListener::bind(addr) {
-        Ok(l) => l,
-        Err(e) => panic!("{:?}", e),
-    };
+    let listener = TcpListener::bind(addr)?;
 
-    println!("\nServer listening on http://localhost:3000/");
+    info!("Server listening on http://localhost:3000/");
 
     for stream in listener.incoming() {
         match stream {
             Ok(s) => {
                 thread::spawn(|| {
-                    let _ = handle_request(s);
+                    match handle_request(s) {
+                        Ok(_) => (),
+                        Err(e) => error!("Error while processing request:{}", e),
+                    }
                 });
             },
             Err(e) => {
-                println!("error: {:?}", e);
+                error!("error: {:?}", e);
             }
         };
     }
+
+    Ok(())
 }
